2435ec0b7c0a7130a81267b1cf4def497c888109
Tatyana <tatyanavv56@gmail.com>
2022-12-08 01:10:41 +0500

    Итоговая работа v.2

--- Итоговая работа/модуль1.py
+++ Итоговая работа/модуль1.py
@@ -1,31 +1,40 @@
 #ver2
 from pprint import pprint
-from bs4 import BeautifulSoup
-from rutermextract import TermExtractor
-import openpyxl
-from tkinter import *
-import requests as req
+from bs4 import BeautifulSoup               #для распознавания html
+from rutermextract import TermExtractor     #для распознавания ключевых фраз
+import openpyxl                             #для работы с экселем         
+from tkinter import *                       #для интерфейса                     
+import requests as req                      #для работы с запросами
 import urllib.request
 import os
 
 WORDS=['тип антенны', 'диапазон частот', 'выходная мощность', 'мощность на входе', 
        'высота установки', 'азимут', 'тип модуляции', 'усиления', 'ширина ДН']
 
+#функция для открытия файла экселя (после распознавания)
 def open_table():
     os.startfile('table.xlsx')
 
 def openSZZ():
     s=[]
+    #получаем текст заключения от сервера
     resp = req.get(txt.get())
-    soup = BeautifulSoup(resp.text, 'xml')
+    #распознаем в нем теги
+    soup = BeautifulSoup(resp.text, 'lxml')
+    #открываем эксель для записи
     wb = openpyxl.load_workbook('table.xlsx')
     sheet = wb.active
     term_extractor = TermExtractor()
+    #назначаем список для хранения характеристик
     PRTO = [[]]
+    #внутри тега <p> ищем нужные фразы (характеристики)
     for tag in soup.find_all("p"):
         n=0
+        #достаем название объекта
         if tag.text.find("№")>0:
                 bs_name = tag.text
+                print(bs_name)
+        #достаем характеристики
         if tag.text.find("передающ")>0:
             t = tag.text.lower()
             for i in t.split('.-'):
@@ -43,27 +52,30 @@ def openSZZ():
                             #    s.append(k[k.find('Вт')-4:k.find('Вт')-1])
                                 PRTO[n].append({'out_power': k[k.find('Вт')-4:k.find('Вт')-1]})
                             if term.normalized.find('вход')>-1:
-                                #sheet.cell(row = n, column = 5).value = j[j.find('мощность на входе'):]
+                                #записываем значение в ячейку экселя
+                                sheet.cell(row = n+2, column = 5).value = j[j.find('мощность на входе'):]
+                                #записываем значение в характеристики ПРТО
                                 PRTO[n].append({'in_power': k[k.find('Вт')-4:k.find('Вт')-1]})
                             if term.normalized.find('высота')>-1:
-                                #sheet.cell(row = n, column = 6).value = j[j.find('высота подвеса'):]
+                                sheet.cell(row = n+2, column = 6).value = j[j.find('высота подвеса'):]
                                 #PRTO[n]['H'] = i[i.find('высота подвеса'):]
                                 pass
                             if term.normalized.find('азимут')>-1:
-                                #sheet.cell(row = n, column = 7).value = j[j.find('азимут'):]
+                                sheet.cell(row = n+2, column = 7).value = j[j.find('азимут'):]
                                 pass
                             if term.normalized.find('ширина ДН')>-1:
-                                #sheet.cell(row = n, column = 9).value = j[j.find('ширина ДН'):]
+                                sheet.cell(row = n+2, column = 9).value = j[j.find('ширина ДН'):]
                                 pass
                         #if k.find('модуляция')>-1:
                             #sheet.cell(row = n, column = 10).value = j[j.find('модуляция'):]
                             #PRTO[n].append({'mod': k[k.find('-')+1:k.find('.')-1]})
                         if k.find('усиления')>-1:
-                            #sheet.cell(row = n, column = 8).value = j[j.find('усиления'):]
+                            sheet.cell(row = n+2, column = 8).value = j[j.find('усиления'):]
                             PRTO[n].append({'Gain': k[k.find('дБи')-8:k.find('дБи')-3]})
                 n+=1
     print(s)
     pprint(PRTO)
+    #сохраняем эксель с данными
     wb.save('table.xlsx')
 
 def BS_name(text):
@@ -111,12 +123,14 @@ def to_excel(prto):
     sheet.cell(row = n, column = 2).value = freq(i)
     pass
 
+#работа с интерфейсом
+#создаем окошко
 root=Tk()
 root.title("Реестр СЗЗ")
 root.geometry("1000x500")
 
     
-
+#Кнопочки - надписи
 #but1=Button(root,text="Загрузить данные реестра",command = download) 
 #but1.grid(column=0, row=0)
 butsearch=Button(root,text="Выполнить анализ",command = openSZZ)
